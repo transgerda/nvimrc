e0787b29b7a09cf78b4e9a3c4f9bd75e71b2b829
transgerda <info@martijnvanboven.nl>
2026-01-10 12:34:35 +0100

    laat van late nacht

---
+++ lua/plugins/lsp/roslyn.lua
@@ -0,0 +1,56 @@
+return   { "seblyng/roslyn.nvim",
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
+    ft = { "cs", "razor" },
+    opts = {
+        -- Path to the .NET SDK you want Roslyn to use
+        dotnet_root = "/nix/store/hbm4r1n5aa3wca0d2960p6nyxv1l6c0x-dotnet-sdk-9.0.112/share/dotnet",
+    
+        -- Make Roslyn pick the project root based on .csproj or .sln
+        root_dir = function(fname)
+          return require("lspconfig.util").root_pattern("*.csproj", "*.sln")(fname) or vim.fn.getcwd()
+        end,
+    
+        -- optional: enable analyzers, editorconfig support
+        analyzers = true,
+        enable_editorconfig_support = true,
+    },
+    config = function()
+            -- Use one of the methods in the Integration section to compose the command.
+            local cmd = {}
+
+            vim.lsp.config("roslyn", {
+                cmd = cmd,
+                settings = {
+                    ["csharp|inlay_hints"] = {
+                        csharp_enable_inlay_hints_for_implicit_object_creation = true,
+                        csharp_enable_inlay_hints_for_implicit_variable_types = true,
+
+                        csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+                        csharp_enable_inlay_hints_for_types = true,
+                        dotnet_enable_inlay_hints_for_indexer_parameters = true,
+                        dotnet_enable_inlay_hints_for_literal_parameters = true,
+                        dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+                        dotnet_enable_inlay_hints_for_other_parameters = true,
+                        dotnet_enable_inlay_hints_for_parameters = true,
+                        dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+                        dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+                        dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+                    },
+                    ["csharp|code_lens"] = {
+                        dotnet_enable_references_code_lens = true,
+                    },
+                },
+            })
+            vim.lsp.enable("roslyn")
+        end,
+        init = function()
+            -- We add the Razor file types before the plugin loads.
+            vim.filetype.add({
+                extension = {
+                    razor = "razor",
+                    cshtml = "razor",
+                },
+            })
+    end,
+}
---
+++ lua/plugins/utils/mason.lua
@@ -0,0 +1,30 @@
+return {
+    "williamboman/mason.nvim",
+    opts = {
+      registries = {
+        "github:mason-org/mason-registry",
+        "github:Crashdummyy/mason-registry",
+      },
+      ensure_installed = {
+        "lua-language-server",
+        
+        "xmlformatter",
+        "csharpier",
+        "prettier",
+      
+        "stylua",
+        "bicep-lsp",
+        "html-lsp",
+        "css-lsp",
+        "eslint-lsp",
+        "typescript-language-server",
+        "json-lsp",
+        "rust-analyzer",
+
+        -- !
+        "roslyn",
+        -- "csharp-language-server",
+        -- "omnisharp",
+      }
+    }
+  }
