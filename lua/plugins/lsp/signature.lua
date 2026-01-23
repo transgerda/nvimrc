145da186ae9f295311956fac9f09da114cc70717
transgerda <info@martijnvanboven.nl>
2026-01-23 10:19:09 +0100

    changes

--- lua/plugins/lsp/signature.lua
+++ lua/plugins/lsp/signature.lua
@@ -3,15 +3,15 @@ return {
     event = "VeryLazy",
     opts = {},
     config = function(_, opts)
-        --require("lsp_signature").setup(opts)
+        require("lsp_signature").setup(opts)
 
         -- Attach only for desired filetypes
-        local allowed_filetypes = { "lua", "rust", "ts", "js", "php" }
-        local ft = vim.bo.filetype
-
-        if vim.tbl_contains(allowed_filetypes, ft) then
-            require("lsp_signature").setup(opts)
-        end
+        -- local allowed_filetypes = { "lua", "rust", "ts", "js", "php" }
+        -- local ft = vim.bo.filetype
+        --
+        -- if vim.tbl_contains(allowed_filetypes, ft) then
+        --     require("lsp_signature").setup(opts)
+        -- end
     end,
 }
 
